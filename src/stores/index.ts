7216d0105d82bf97b71f762b59d184e08e5dd85c
Johnson Chen <chnejohnson@gmail.com>
2023-11-28 17:15:06 +0800

    chore: update and reconfigure dependencies

--- src/stores/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './useApp'
-export * from './useRecipient'
-export * from './useUser'
-export * from './useRound'
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import nodeStdlibBrowser from 'node-stdlib-browser'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import inject from '@rollup/plugin-inject'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
@@ -40,32 +39,9 @@ export default defineConfig({
 		}),
 		Icons(),
 		VueI18nPlugin({}),
+		nodePolyfills(),
 	],
 	resolve: {
-		alias: { '@': path.resolve(__dirname, 'src'), ...nodeStdlibBrowser },
-	},
-	optimizeDeps: {
-		esbuildOptions: {
-			target: 'esnext', // to enable nable Big integer literals
-			inject: [require.resolve('node-stdlib-browser/helpers/esbuild/shim')],
-		},
-	},
-	build: {
-		target: 'esnext', // to enable Big integer literals
-		commonjsOptions: {
-			transformMixedEsModules: true, // to enable @walletconnect/web3-provider which has some code in CommonJS
-		},
-		rollupOptions: {
-			plugins: [
-				inject({
-					global: [require.resolve('node-stdlib-browser/helpers/esbuild/shim'), 'global'],
-					process: [
-						require.resolve('node-stdlib-browser/helpers/esbuild/shim'),
-						'process',
-					],
-					Buffer: [require.resolve('node-stdlib-browser/helpers/esbuild/shim'), 'Buffer'],
-				}),
-			],
-		},
+		alias: { '@': path.resolve(__dirname, 'src') },
 	},
 })
