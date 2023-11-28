33397c2adc6e20d4e5b7414b1c9674ce80a5a769
Johnson Chen <chnejohnson@gmail.com>
2023-11-28 14:39:59 +0800

    dev: delete /api folder

--- src/api/__test__/user.spec.ts
+++
@@ -1,7 +0,0 @@
-import { expect } from 'vitest'
-import { isVerifiedUser } from '@/api/user'
-import { ethers } from 'ethers'
-
-describe('users', () => {
-	it('isVerifiedUser - should throw error if the addesses are invalid', async () => {})
-})
